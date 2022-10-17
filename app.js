a4f999603ca57508dd132943935f0c2f975767d9
goodoljosh <findley_joshua1@columbusstate.edu>
2022-10-17 18:01:07 +0000

    Testing website, trouble connecting

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var axios = require('axios');
 
 app.get('/', function(req,res){
-    res.send('');
-})
+     res.send('https://xkcd.com/614/info.0.json')
+    //api call by comic number https://xkcd.com/614/info.0.json number (614) randomly generated
+    //min 1 max 2682
+ })
 
 app.listen(3000, function(){
     console.log('App listening on port 3000')
