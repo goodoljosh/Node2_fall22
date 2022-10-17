7d96b86e44ab76f813394d654c23069ef8f909c6
goodoljosh <findley_joshua1@columbusstate.edu>
2022-10-17 18:20:33 +0000

    Created basic skeleton for node 2

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var axios = require('axios');
 
 app.get('/', function(req,res){
      res.send('https://xkcd.com/614/info.0.json')
+     
     //api call by comic number https://xkcd.com/614/info.0.json number (614) randomly generated
     //min 1 max 2682
  })
+
 
 app.listen(3000, function(){
     console.log('App listening on port 3000')
