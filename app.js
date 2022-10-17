27c2a2bbf7ffd65013ca433af9a81b7c7abc4fe0
goodoljosh <findley_joshua1@columbusstate.edu>
2022-10-17 19:25:37 +0000

    Added stylesheets

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 var express = require('express');
 var app = express();
 var axios = require('axios');
+var mongoose = require('mongoose')
 
-app.get('/', function(req,res){
-     res.send('https://xkcd.com/614/info.0.json')
-     
-    //api call by comic number https://xkcd.com/614/info.0.json number (614) randomly generated
-    //min 1 max 2682
+app.use('/static', express.static("public"));
+app.use(express.urlencoded({extended: true}))
+app.set("view engine", "ejs")
+
+// app.get('/', function(req,res){
+//     axios.get('https://xkcd.com/614/info.0.json').then(function(response){
+//         Todo.find(function(err, todo){
+//             if(err){
+//                 res.json({"Error:" : err})
+//             } else {
+//                 res.render("todo.ejs", {todoList: todo, comicData: response.data});
+//             }
+//         })
+//     }).catch(function(error){
+//         res.json({"Error:" : error})
+//     })
+//     //api call by comic number https://xkcd.com/614/info.0.json number (614) randomly generated
+//     //min 1 max 2682
+//  })
+ 
+
+ app.get('/', function(req,res){
+    res.render('todo.ejs')
+ })
+
+ app.get('/', function(req,res){
+    res.send("Hello World")
  })
 
 
