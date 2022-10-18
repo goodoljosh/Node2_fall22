ccf0d3eabd0214a9d08875e9eb5285b09960579d
goodoljosh <findley_joshua1@columbusstate.edu>
2022-10-18 16:20:47 +0000

    Deployed picture sucessfully with mongo

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
 var express = require('express');
 var app = express();
 var axios = require('axios');
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+//const todoModel = require('./models/todo.model');
+//const Todo = require('./models/todo.model');
 
-app.use('/static', express.static("public"));
-app.use(express.urlencoded({extended: true}))
-app.set("view engine", "ejs")
+app.use('/static', express.static("public")); 
+app.set("view engine","ejs");
+const Todo = require('./models/todo.model');
+const mongoDB = "mongodb+srv://findley_joshua1:cyjQ2HdqfrCVst0m@cluster0.s3catnk.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(mongoDB)
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error' , console.error.bind(console, "MongoDb connection error: "))
 
-// app.get('/', function(req,res){
-//     axios.get('https://xkcd.com/614/info.0.json').then(function(response){
-//         Todo.find(function(err, todo){
-//             if(err){
-//                 res.json({"Error:" : err})
-//             } else {
-//                 res.render("todo.ejs", {todoList: todo, comicData: response.data});
-//             }
-//         })
-//     }).catch(function(error){
-//         res.json({"Error:" : error})
-//     })
-//     //api call by comic number https://xkcd.com/614/info.0.json number (614) randomly generated
-//     //min 1 max 2682
-//  })
+app.get('/', function(req,res){
+   let comicData ={}
+   axios.get('https://xkcd.com/614/info.0.json').then(function(response){
+      comicData=response.data
+    })
+   Todo.find(function(err,todo){
+      console.log(todo)
+      if(err){
+         res.json({"Error: ": err})
+      } else {
+         res.render('todo.ejs', {todoList: todo, comicData: comicData});
+      }
+   })
+ })
  
 
- app.get('/', function(req,res){
-    res.render('todo.ejs')
- })
+//  app.get('/', function(req,res){
+//     res.render('todo.ejs')
+//  })
 
- app.get('/', function(req,res){
-    res.send("Hello World")
- })
+//  app.get('/', function(req,res){
+//     res.send("Hello World")
+//  })
 
 
 app.listen(3000, function(){
